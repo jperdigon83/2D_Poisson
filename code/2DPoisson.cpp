8d69189873ce9ea9fcf811a77dbc234181be03de
jperdigon <jeremy.perdigon@oca.eu>
2021-01-27 09:44:26 +0100

    Including FFT and DST functions

--- code/2DPoisson.cpp
+++ code/2DPoisson.cpp
@@ -173,16 +173,115 @@ private:
   
 };
 
+
+
+inline double ** FFT(std::size_t N, double * &x){
+
+  double ** z = new double*[2]; // Complex vector
+  z[0] = new double[N]; // real part
+  z[1] = new double[N]; // Imaginary part
+  
+  if(N==1){
+    z[0][0] = x[0];
+    z[1][0] = 0;
+    
+  }else{
+    
+    double x_even[N/2];
+    double x_odd[N/2];
+    
+    for(std::size_t i =0; i<(N/2); ++i){
+      x_even[i] = x[2*i];
+      x_odd[i] = x[2*i+1];
+    }
+
+    double ** z_even = new double*[2];
+    z_even[0] = new double[N/2]; // real part
+    z_even[1] = new double[N/2]; // Imaginary part
+
+    double ** z_odd = new double*[2];
+    z_odd[0] = new double[N/2]; // real part
+    z_odd[1] = new double[N/2]; // Imaginary part
+
+    z_even = FFT( N/2, x_even);
+    z_odd = FFT( N/2, x_odd);
+
+    for(std::size_t k =0; k<(N/2); ++k){
+      
+      z[0][k] = z_even[0][k] + cos(2*M_PI*k/N)*z_odd[0][k] + sin(2*M_PI*k/N)*z_odd[1][k];
+      z[1][k] = z_even[1][k] + cos(2*M_PI*k/N)*z_odd[1][k] - sin(2*M_PI*k/N)*z_odd[0][k];
+
+      z[0][k+N/2] = z_even[0][k] - cos(2*M_PI*k/N)*z_odd[0][k] - sin(2*M_PI*k/N)*z_odd[1][k];
+      z[1][k+N/2] = z_even[1][k] - cos(2*M_PI*k/N)*z_odd[1][k] + sin(2*M_PI*k/N)*z_odd[0][k];
+      
+    }
+
+    delete [] z_even[0];
+    delete [] z_even[1];   
+    delete [] z_even;
+
+    delete [] z_odd[0];
+    delete [] z_odd[1];   
+    delete [] z_odd;  
+  }
+
+  return z;
+  
+}
+
+inline void DST(std::size_t N, double * &y, double * &x){
+
+  int Nl = 2*N+2;
+  
+  double * xl = new double[Nl];
+  xl[0] = 0;
+  xl[N+1] = 0;
+  for(std::size_t i=1; i<N+1; ++i){
+    xl[i] = x[i-1];
+    xl[N+1+i] = -x[N-i];
+  }
+  
+  double ** z = FFT(Nl,xl);
+
+  for(std::size_t i=0; i<N; ++i){
+    y[i] = -0.5*z[1][i+1];
+  }
+
+  delete [] z[0];
+  delete [] z[1];   
+  delete [] z;
+  delete [] xl;
+
+}
+
+
 int main(int argc, char** argv){
   
-  const std::size_t M = atoi(argv[1]);
-  const std::size_t Lx = atof(argv[2]);
-  const std::size_t Ly = atof(argv[3]);
+  //const std::size_t M = atoi(argv[1]);
+  //const std::size_t Lx = atof(argv[2]);
+  //const std::size_t Ly = atof(argv[3]);
+  //TwoDPoisson test(M, Lx, Ly);
+  //const char* filename = "../result.dat";
+  //test.saveU(filename);
   
-  TwoDPoisson test(M, Lx, Ly);
+  // Test FFT
 
-  const char* filename = "../result.dat";
-  test.saveU(filename);
+  std::size_t N = 63;
+  
+  double x[N];
+  double * y = new double[N];
+  for(std::size_t i =0; i<N; ++i){
+    x[i] = 1.0;
+  }
+  
+  DST(N,y,x);
 
+  for(std::size_t i =0; i<N; ++i){
+    std::cout << i << " " << x[i] << " " << y[i] <<  std::endl;
+  }
+
+  delete [] y;
+  
+ 
   return 0; 
 }
